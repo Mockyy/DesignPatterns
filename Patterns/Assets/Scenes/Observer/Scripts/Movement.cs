01159fe0d0be64c7479bc75d24972540440d89dc
Mockyy <32643968+Mockyy@users.noreply.github.com>
2021-01-13 11:32:07 +0100

    Observer Pattern

---
+++ Patterns/Assets/Scenes/Observer/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Simple mouvement du personnage avec ZQSD
+public class Movement : MonoBehaviour
+{
+    private float vertical;
+    private float horizontal;
+    [SerializeField] private int speed = default;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(new Vector3(horizontal, vertical) * Time.deltaTime * speed);
+    }
+}
